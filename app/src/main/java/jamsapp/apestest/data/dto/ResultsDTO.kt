ab8b357a9b9d640e2e0825e383fd859c5753e061
Anders <jamsmurillo92@gmail.com>
2019-09-22 22:51:38 -0500

    Modify ResultsDTO and corresponding model according to received response

--- app/src/main/java/jamsapp/apestest/data/dto/ResultsDTO.kt
+++ app/src/main/java/jamsapp/apestest/data/dto/ResultsDTO.kt
@@ -7,9 +7,9 @@ class ResultsDTO (
 	@SerializedName("api_detail_url")
 	var api_detail_url : String? = null,
 	@SerializedName("box_office_revenue")
-	var box_office_revenue : Int? = 0,
+	var box_office_revenue : String? = null,
 	@SerializedName("budget")
-	var budget : Int? = 0,
+	var budget : String? = null,
 	@SerializedName("date_added")
 	var date_added : String? = null,
 	@SerializedName("date_last_updated")
@@ -23,25 +23,25 @@ class ResultsDTO (
 	@SerializedName("has_staff_review")
 	var has_staff_review : String? = null,
 	@SerializedName("id")
-	var id : Int? = 0,
+	var id : String? = null,
 	@SerializedName("image")
 	var image : ImageDTO? = null,
 	@SerializedName("name")
 	var name : String? = null,
-	@SerializedName("producers")
-	var producers : String? = null,
+	/*@SerializedName("producers")
+	var producers : String? = null,*/
 	@SerializedName("rating")
 	var rating : String? = null,
 	@SerializedName("release_date")
 	var release_date : String? = null,
 	@SerializedName("runtime")
-	var runtime : Int? = 0,
+	var runtime : String? = null,
 	@SerializedName("site_detail_url")
 	var site_detail_url : String? = null,
 	@SerializedName("studios")
 	var studios : List<StudiosDTO>? = null,
 	@SerializedName("total_revenue")
-	var total_revenue : Int? = 0,
+	var total_revenue : String? = null,
 	@SerializedName("writers")
 	var writers : List<WritersDTO>? = null
 )
